62db38d9cf99bc87e66815e84807700ca267271e
Spidey <65277376+Spidey0918@users.noreply.github.com>
2022-12-19 17:06:49 +0800

    Update lightgbm_prediction.py

--- lgbm_trainer/lightgbm_prediction.py
+++ lgbm_trainer/lightgbm_prediction.py
@@ -1,26 +1,7 @@
-from string import digits
-import numpy as np
 import pandas as pd
 import lightgbm as lgbm
 from parsing_utility import index_range_parser
-from sklearn.metrics import classification_report, confusion_matrix
 import sys
-
-headers = sys.argv[7].split(',')
-threshold = eval(sys.argv[8])
-
-
-def compute_metrics(df, label_col, pred_col):
-    labels = df[label_col].tolist()
-    pred_labels = df[pred_col].tolist()
-    classification_metric = classification_report(
-        labels, pred_labels, digits=4)
-    confusion_mat = confusion_matrix(labels, pred_labels)
-
-    print('Metrics Report: ')
-    print(classification_metric)
-    print('Confustion Matrix: ')
-    print(confusion_mat)
 
 
 def lightgbm_prediction(model_path, features):
@@ -29,19 +10,13 @@ def lightgbm_prediction(model_path, features):
 
 
 if __name__ == '__main__':
-    input_data = pd.read_csv(sys.argv[1], names=headers, sep='\t')
-    print("Test Data Size: ", input_data.shape[0])
+    input_data = pd.read_csv(sys.argv[1], header=None, sep='\t')
     feature_indices = index_range_parser(sys.argv[3])
     keep_columns_indices = index_range_parser(sys.argv[5])
     if sys.argv[4] != '-':
         category_ind = index_range_parser(sys.argv[4])
-        input_data.iloc[:, category_ind] = input_data.iloc[:,
-                                                           category_ind].astype('category')
-    predictions = lightgbm_prediction(
-        sys.argv[2], input_data.iloc[:, feature_indices])
+        input_data.iloc[:,category_ind] = input_data.iloc[:,category_ind].astype('category')
+    predictions = lightgbm_prediction(sys.argv[2], input_data.iloc[:, feature_indices])
     output_data = input_data.iloc[:, keep_columns_indices].copy()
     output_data['Score'] = predictions
-    output_data['IsShown_Pred'] = np.where(
-        output_data['Score'] > threshold, 1, 0)
-    compute_metrics(output_data, 'IsShown', 'IsShown_Pred')
     output_data.to_csv(sys.argv[6], sep='\t', index=False)
