16eb95e94916e6fbb2778ff3ae3e547034f821d1
X <70434123+tmxz2388@users.noreply.github.com>
2023-01-08 00:41:09 +0800

    Update XGBoost_prediction.py

--- xgboost trainer/XGBoost_prediction.py
+++ xgboost trainer/XGBoost_prediction.py
@@ -1,6 +1,10 @@
+# START: OWN CODE
 import pandas as pd
 import xgboost as xgb
 import sys
+import numpy as np
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
 
 
 def index_range_parser(range_string):
@@ -18,8 +22,20 @@ def index_range_parser(range_string):
     return expanded_range
 
 
+def compute_metrics(df, label_col, pred_col):
+    labels = label_col.tolist()
+    pred_labels = df[pred_col].tolist()
+    classification_metric = classification_report(
+    labels, pred_labels, digits=4)
+    confusion_mat = confusion_matrix(labels, pred_labels)
+
+    print("classification_report:\n", classification_metric)
+    print("confusion matrix:\n", confusion_mat)
+
+
 def xgboost_prediction(model_path, features):
     trained_model = pd.read_pickle(model_path)
+    print(trained_model.predict_proba(features)[:, 1])
     return trained_model.predict_proba(features)[:, 1]
 
 
@@ -31,7 +47,13 @@ if __name__ == '__main__':
         category_ind = index_range_parser(sys.argv[4])
         input_data.iloc[:, category_ind] = input_data.iloc[:, category_ind].astype('category')
     predictions = xgboost_prediction(sys.argv[2], input_data.iloc[:, feature_indices])
-    print(predictions)
+    # print(predictions)
     output_data = input_data.iloc[:, keep_columns_indices].copy()
     output_data['Score'] = predictions
+    threshold = 0.132222  # from cal_pr_auc.py do not forget to change it
+    output_data['IsShown_Predict'] = np.where(output_data['Score'] > threshold, 1, 0)
+    print(output_data)
+    compute_metrics(output_data, output_data.iloc[:, 2], 'IsShown_Predict')
     output_data.to_csv(sys.argv[6], sep='\t', index=False)
+    
+# END: OWN CODE
