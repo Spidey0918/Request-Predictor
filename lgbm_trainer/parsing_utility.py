5d85eb5201e095f13f8d8ca7829251c51f4b31c7
Spidey <445906160@qq.com>
2023-01-08 17:05:16 +0800

    Update parsing_utility.py

--- lgbm_trainer/parsing_utility.py
+++ lgbm_trainer/parsing_utility.py
@@ -1,3 +1,4 @@
+# START: OWN CODE
 def index_range_parser(range_string):
     ranges = [index.strip() for index in range_string.split(',')]
     expanded_range = []
@@ -11,3 +12,4 @@ def index_range_parser(range_string):
             end_ind = int(indices[1])
         expanded_range.extend(range(start_ind, end_ind + 1))
     return expanded_range
+# END: OWN CODE
